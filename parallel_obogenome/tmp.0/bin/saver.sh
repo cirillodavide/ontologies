c9f7bd19827e8a32f456add3496b6703111cf999
cirillodavide <davide.cirillo@bsc.es>
2017-03-22 18:22:42 +0100

    added some parllelization tasks

---
+++ parallel_obogenome/tmp.0/bin/saver.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+while [ 1 ]; do
+    perl bin/saver.pl $1
+    sleep 5
+done
---
+++ parallel_obogenome/tmp.1/lib/parseinput/parseinput.pm
@@ -0,0 +1,29 @@
+package parseinput::parseinput;
+use strict;
+use warnings;
+use Path::Tiny;
+use List::MoreUtils 'uniq';
+
+use Exporter 'import';
+our @EXPORT_OK = qw/parseinput/;
+
+sub parseinput {
+
+	my @genes;
+	
+	my $in = $_[0];
+	$in =~ s/\s*$//;
+	if(path($in) -> is_file){
+		open my $fh, path($in) or die "$!";
+		while(<$fh>){
+			next if /^\s*$/;
+			$_ =~ s/^\s+|\s+$//g;
+			push @genes, $_;
+		}
+		close $fh;
+		}else{
+			push @genes, split/\s+/,$in;
+			@genes = uniq @genes;
+		}
+		return \@genes;
+	}
